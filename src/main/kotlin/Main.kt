11b634a8cf8894affef1499a2ac6fe5b21f29a08
lrezendes <lrezendes@student.bridgew.edu>
2023-02-09 11:43:21 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    lectures()
+}
+
+fun lectures() {
+    val pairExample = Pair("Tom", "Jerry")
+    println(pairExample)
+
+    val mapExample = mapOf("Tom" to "Cat", "Jerry" to "Mouse")
+    println("$mapExample\n")
+}
